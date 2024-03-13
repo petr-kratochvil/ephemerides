9877caa179e6c41ad87788a88b5238a04ba795ce
Petr Kratochvíl <1684874+petr-kratochvil@users.noreply.github.com>
2024-03-13 14:14:09 +0100

    CORS headers

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ app.use(express.json());
 
 // Handle OPTIONS preflight requests
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Content-type");
     res.send();
   } else {
