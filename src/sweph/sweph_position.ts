ddcbf04e25858727f9c6c8fe0b463192d944bfcb
Petr Kratochvíl <1684874+petr-kratochvil@users.noreply.github.com>
2025-02-09 12:53:22 +0100

    sweph_position refactor

--- src/sweph/sweph_position.ts
+++ src/sweph/sweph_position.ts
@@ -1,26 +1,34 @@
 import sweph, { constants } from "sweph";
-import { EphDate, EphObjectPosition, SwephError } from "../types";
+import { BodyId, EphDate, EphObjectPosition, SwephError } from "../types";
 import { swephConfig } from "../config";
 
 export function sweph_position(date: EphDate): EphObjectPosition[] {
-  const julday_ut = sweph.julday(
+  const julday_ut = get_julday(date);
+
+  return swephConfig.bodies.map((body) => {
+   return sweph_body_position(julday_ut, body);
+  });
+}
+
+export function get_julday(date: EphDate): number {
+  return sweph.julday(
     date.year,
     date.month,
     date.day,
     date.hour,
     constants.SE_GREG_CAL
   );
+}
 
-  return swephConfig.bodies.map((body) => {
-    const calc = sweph.calc_ut(julday_ut, body, swephConfig.flag);
-    if (calc.error) {
-      throw { body, error: calc.error } as SwephError;
-    }
-    return {
-      type: "body",
-      bodyId: body,
-      position: calc.data[0],
-      speed: calc.data[3],
-    } as EphObjectPosition;
-  });
+export function sweph_body_position(julday_ut: number, body: BodyId): EphObjectPosition {
+  const calc = sweph.calc_ut(julday_ut, body, swephConfig.flag);
+  if (calc.error) {
+    throw { body, error: calc.error } as SwephError;
+  }
+  return {
+    type: "body",
+    bodyId: body,
+    position: calc.data[0],
+    speed: calc.data[3],
+  } as EphObjectPosition;
 }
